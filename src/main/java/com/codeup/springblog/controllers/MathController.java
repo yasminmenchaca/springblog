13b06c54ceaa96b7595a582128acb99ded981eb9
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-03-13 13:57:08 -0500

    -

--- src/main/java/com/codeup/springblog/controllers/MathController.java
+++ src/main/java/com/codeup/springblog/controllers/MathController.java
@@ -12,25 +12,25 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class MathController {
     @RequestMapping(path = "/add/{num1}/and/{num2}", method = RequestMethod.GET)
     @ResponseBody
-    public String add(@PathVariable double num1, @PathVariable double num2) {
-        return "" + (num1 + num2);
+    public double add(@PathVariable double num1, @PathVariable double num2) {
+        return (num1 + num2);
     }
 
     @RequestMapping(path = "/subtract/{num1}/and/{num2}", method = RequestMethod.GET)
     @ResponseBody
-    public String subtract(@PathVariable double num1, @PathVariable double num2) {
-        return "" + (num1 - num2);
+    public double subtract(@PathVariable double num1, @PathVariable double num2) {
+        return (num1 - num2);
     }
 
     @RequestMapping(path = "/multiply/{num1}/and/{num2}", method = RequestMethod.GET)
     @ResponseBody
-    public String multiply(@PathVariable double num1, @PathVariable double num2) {
-        return "" + (num1 * num2);
+    public double multiply(@PathVariable double num1, @PathVariable double num2) {
+        return (num1 * num2);
     }
 
     @RequestMapping(path = "/divide/{num1}/and/{num2}", method = RequestMethod.GET)
     @ResponseBody
-    public String divide(@PathVariable double num1, @PathVariable double num2) {
-        return "" + (num1 / num2);
+    public double divide(@PathVariable double num1, @PathVariable double num2) {
+        return (num1 / num2);
     }
 }
