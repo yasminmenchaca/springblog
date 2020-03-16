9fab6452e0266d082e1a72c30c2b484abfcb69b4
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-03-16 11:29:03 -0500

    -

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -1,51 +1,49 @@
 package com.codeup.springblog.controllers;
 
-
 import com.codeup.springblog.models.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.thymeleaf.model.IModel;
 
 import java.util.ArrayList;
 
 @Controller
 public class PostController {
 
-    // model all posts
     @GetMapping("/posts")
-    public String viewIndexPage(Model model) {
-        ArrayList<Post> posts = new ArrayList<>();
-        posts.add(new Post(1, "Post 1", "Body 1"));
-        posts.add(new Post(2, "Post 2", "Body 2"));
-        posts.add(new Post(3, "Post 3", "Body 3"));
-        model.addAttribute("Posts", posts);
-        return "/posts/index";
+    public String getPosts(Model model){
+        ArrayList<Post> postList = new ArrayList<>();
+        postList.add(new Post(2, "Second Post", "askdfhkashdfkjahsdf"));
+        postList.add(new Post(3, "Third Post", "some more text..."));
+
+        model.addAttribute("posts", postList);
+        return "posts/index";
     }
 
-    // model a particular post by id
     @GetMapping("/posts/{id}")
-    public String getSinglePost(@PathVariable int id, Model model) {
-        Post post = new Post(id,"Post Title", "Post Body");
-        model.addAttribute("title", post.getTitle());
-        model.addAttribute("body", post.getBody());
-        return "/posts/show";
+    public String getPost(@PathVariable int id, Model model){
+        Post post1 = new Post(id, "Europa's First Post", "Remote Learning Today!");
+        model.addAttribute("title", post1.getTitle());
+        model.addAttribute("body", post1.getBody());
+        return "posts/show";
     }
 
-    // model the "Create Post" form
     @GetMapping("/posts/create")
     @ResponseBody
-    public String getCreatePostForm() {
-        return "model the form for creating a post";
+    public String getCreatePostForm(){
+        return "view the form for creating a post";
     }
 
-    // Submit the "Create Post" form
     @PostMapping("/posts/create")
     @ResponseBody
-    public String createPost() {
+    public String createPost(){
         return "create a new post";
     }
 
+    @RequestMapping(path="/posts", method=RequestMethod.DELETE)
+    @ResponseBody
+    public String delete(){
+        return "DELETE!!";
+    }
 }
