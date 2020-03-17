6811482601cddbf7f8697c09c5d99baacaa0b94e
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-03-17 14:58:12 -0500

    -

--- seeder.sql
+++ seeder.sql
@@ -1,33 +1,23 @@
-# USE dogs_db;
-#
-# insert into dogs (age, name, reside_state) values (2, 'Zandra', 'TX');
-# insert into dogs (age, name, reside_state) values (3, 'Josey', 'NC');
-# insert into dogs (age, name, reside_state) values (9, 'Nikki', 'CA');
-# insert into dogs (age, name, reside_state) values (5, 'Freddi', 'FL');
-# insert into dogs (age, name, reside_state) values (11, 'Gino', 'WA');
-# insert into dogs (age, name, reside_state) values (15, 'Tess', 'TX');
-# insert into dogs (age, name, reside_state) values (2, 'Davidde', 'CA');
-# insert into dogs (age, name, reside_state) values (13, 'Gill', 'MO');
-# insert into dogs (age, name, reside_state) values (1, 'Eliza', 'TX');
-# insert into dogs (age, name, reside_state) values (7, 'Cindelyn', 'VA');
-# insert into dogs (age, name, reside_state) values (8, 'Alia', 'AZ');
-# insert into dogs (age, name, reside_state) values (4, 'Michail', 'WA');
-# insert into dogs (age, name, reside_state) values (6, 'Leonie', 'MS');
-# insert into dogs (age, name, reside_state) values (14, 'Garreth', 'AZ');
-# insert into dogs (age, name, reside_state) values (14, 'Gawen', 'PA');
-# insert into dogs (age, name, reside_state) values (7, 'Wilhelmina', 'GA');
-# insert into dogs (age, name, reside_state) values (5, 'Lorraine', 'AL');
-# insert into dogs (age, name, reside_state) values (8, 'Anson', 'TX');
-
 USE posts_db;
 
-insert into posts (title, body) values ('sit amet consectetuer', 'mi in porttitor pede justo eu massa donec dapibus duis at velit eu est congue');
-insert into posts (title, body) values ('in blandit ultrices', 'adipiscing elit proin interdum mauris non ligula pellentesque ultrices phasellus id sapien in');
-insert into posts (title, body) values ('in hac habitasse platea', 'habitasse platea dictumst morbi vestibulum velit id pretium iaculis diam erat');
-insert into posts (title, body) values ('feugiat et eros vestibulum', 'in faucibus orci luctus et ultrices posuere cubilia curae duis');
-insert into posts (title, body) values ('nisl nunc nisl duis', 'id ligula suspendisse ornare consequat lectus in est risus auctor sed');
-insert into posts (title, body) values ('nam congue risus semper', 'turpis integer aliquet massa id lobortis convallis tortor risus dapibus augue vel accumsan tellus nisi eu orci mauris lacinia sapien');
-insert into posts (title, body) values ('pellentesque ultrices phasellus', 'velit vivamus vel nulla eget eros elementum pellentesque quisque porta volutpat erat quisque erat eros viverra eget');
-insert into posts (title, body) values ('viverra pede ac diam', 'molestie hendrerit at vulputate vitae nisl aenean lectus pellentesque eget nunc donec quis orci eget');
-insert into posts (title, body) values ('id nulla ultrices aliquet', 'blandit ultrices enim lorem ipsum dolor sit amet consectetuer adipiscing elit proin interdum mauris non ligula');
-insert into posts (title, body) values ('sit amet justo morbi', 'felis sed lacus morbi sem mauris laoreet ut rhoncus aliquet pulvinar sed nisl nunc rhoncus dui vel sem');
+insert into posts (title, body)
+values ('in imperdiet et commodo vulputate justo in blandit ultrices enim',
+        'Nulla tellus. In sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus. Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst. Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat. Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem. Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat. Praesent blandit.'),
+       ('metus sapien ut nunc vestibulum',
+        'Nullam varius. Nulla facilisi. Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque. Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus. Phasellus in felis. Donec semper sapien a libero. Nam dui. Proin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius. Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi. Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.'),
+       ('quisque id justo sit amet sapien dignissim vestibulum vestibulum ante',
+        'In eleifend quam a odio. In hac habitasse platea dictumst. Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat. Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem. Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat. Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede. Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.'),
+       ('volutpat erat quisque erat eros viverra eget congue eget semper',
+        'Cras pellentesque volutpat dui. Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae, Mauris viverra diam vitae quam. Suspendisse potenti. Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris. Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis. Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem.'),
+       ('justo morbi ut odio cras mi pede',
+        'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem. Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio. Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.'),
+       ('eu interdum eu tincidunt in',
+        'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum. In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo. Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis. Sed ante. Vivamus tortor. Duis mattis egestas metus. Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.'),
+       ('bibendum morbi non quam nec dui',
+        'Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus. Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem. Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio. Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin interdum mauris non ligula pellentesque ultrices.'),
+       ('amet nunc viverra dapibus nulla suscipit ligula in lacus curabitur',
+        'Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus. Phasellus in felis. Donec semper sapien a libero. Nam dui. Proin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius. Integer ac leo. Pellentesque ultrices mattis odio.'),
+       ('aliquam sit amet diam in',
+        'Etiam pretium iaculis justo. In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus. Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi. Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque. Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus. Phasellus in felis. Donec semper sapien a libero. Nam dui. Proin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis.'),
+       ('pharetra magna ac consequat metus sapien ut nunc vestibulum',
+        'Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus. Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst. Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat. Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem. Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat. Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede. Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.');
--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -6,8 +6,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-
 @Controller
 public class PostController {
 
@@ -33,13 +31,13 @@ public class PostController {
         return "posts/index";
     }
 
-//    @GetMapping("/posts/{id}")
-//    public String getPostById(@PathVariable int id, Model model) {
-//        Post postExample = new Post(id, "Europa's First Post", "Remote Learning Today!");
-//        model.addAttribute("title", postExample.getTitle());
-//        model.addAttribute("body", postExample.getBody());
-//        return "posts/show";
-//    }
+    @GetMapping("/posts/{id}")
+    public String getPostById(@PathVariable long id, Model model) {
+        Post postSing = postsDao.getOne(id);
+        model.addAttribute("title", postSing.getTitle());
+        model.addAttribute("body", postSing.getBody());
+        return "posts/show";
+    }
 
     @GetMapping("/posts/create")
     @ResponseBody
@@ -53,21 +51,21 @@ public class PostController {
         return "create a new post";
     }
 
-    @GetMapping("/posts/update/{id}/{title}")
-    @ResponseBody
-    public String updatePost(@PathVariable long id, @PathVariable String title) {
+    @GetMapping("/posts/edit/{id}")
+//    @ResponseBody
+    public String editPostForm(@PathVariable long id, @PathVariable String title) {
         Post post = postsDao.getOne(id);
         post.setTitle(title);
         postsDao.save(post);
-        return "Updating post";
+        return "redirect:/posts";
     }
 
     @GetMapping("posts/delete/{id}")
-    @ResponseBody
-    public String updatePost(@PathVariable long id) {
+//    @ResponseBody
+    public String deletePost(@PathVariable long id) {
         Post post = postsDao.getOne(id);
         postsDao.delete(post);
-        return "Deleting post";
+        return "redirect:/posts";
     }
 
 }
