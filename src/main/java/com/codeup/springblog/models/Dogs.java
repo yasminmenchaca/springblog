406499625b10dc0c34a9aff9633ef7544926f191
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-03-17 07:51:39 -0500

    in progress

---
+++ src/main/java/com/codeup/springblog/models/Dogs.java
@@ -0,0 +1,58 @@
+package com.codeup.springblog.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="dogs")
+public class Dogs {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(length = 11)
+    private int id;
+
+    @Column(nullable = false, length = 3, unique = true, columnDefinition = "TINYINT(3) UNSIGNED")
+    private int age;
+
+    @Column(nullable = false, length = 200)
+    private String name;
+
+    @Column(columnDefinition = "char(2) default 'XX'")
+    private String resideState;
+
+
+    public Dogs() {
+    };
+
+    public long getId() {
+        return id;
+    }
+
+    public long getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getResideState() {
+        return resideState;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setResideState(String resideState) {
+        this.resideState = resideState;
+    }
+}
