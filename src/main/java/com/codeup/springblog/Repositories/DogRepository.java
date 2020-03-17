3aef7c0d6c932fce4d30e544f3b33ed1bb34305d
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-03-17 10:02:04 -0500

    -

--- src/main/java/com/codeup/springblog/Repositories/DogRepository.java
+++ src/main/java/com/codeup/springblog/Repositories/DogRepository.java
@@ -4,4 +4,6 @@ import com.codeup.springblog.models.Dogs;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DogRepository extends JpaRepository<Dogs, Long> {
+
+    Dogs findByName(String name);
 }
