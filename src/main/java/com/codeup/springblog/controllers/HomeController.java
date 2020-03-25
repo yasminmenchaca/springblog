23b8c8672b652b41e198ddb36cec17ff2fd80e89
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-03-25 13:37:48 -0500

    made changes

--- src/main/java/com/codeup/springblog/controllers/HomeController.java
+++ src/main/java/com/codeup/springblog/controllers/HomeController.java
@@ -4,23 +4,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class HomeController {
+    @GetMapping("/")
+    @ResponseBody
+    public String landing() {
+        return  "This is the landing page!";
+    }
 
     @GetMapping("/home")
     public String welcome() {
         return "home";
     }
 
-//    @GetMapping("/hello/{name}")
-//    public String sayHello(@PathVariable String name, Model model) {
-//        model.addAttribute("name", name);
-//        return "hello";
-//    }
 
 //    @GetMapping("/fruit")
 //    public String fruit(Model model) {
